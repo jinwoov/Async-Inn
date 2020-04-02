a1f3bc7567a0ee2edd3135e2db382794654de257
Jin Kim <jinwoov@gmail.com>
2020-04-02 11:17:18 -0700

    finished lab 13 and added required database

---
+++ AsyncInn/AsyncInn/Migrations/20200402181325_modelsAndTablesBuilt.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class modelsAndTablesBuilt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Amenities",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Amenities", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hotel",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    StreetAddress = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotel", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Layout = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HotelRoom",
+                columns: table => new
+                {
+                    HotelID = table.Column<int>(nullable: false),
+                    RoomID = table.Column<int>(nullable: false),
+                    RoomNumber = table.Column<int>(nullable: false),
+                    PetFriendly = table.Column<bool>(nullable: false),
+                    Rate = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelRoom", x => new { x.HotelID, x.RoomID });
+                    table.ForeignKey(
+                        name: "FK_HotelRoom_Hotel_HotelID",
+                        column: x => x.HotelID,
+                        principalTable: "Hotel",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HotelRoom_Room_RoomID",
+                        column: x => x.RoomID,
+                        principalTable: "Room",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomAmenities",
+                columns: table => new
+                {
+                    RoomID = table.Column<int>(nullable: false),
+                    AmenitiesID = table.Column<string>(nullable: false),
+                    AmenitiesID1 = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomAmenities", x => new { x.AmenitiesID, x.RoomID });
+                    table.ForeignKey(
+                        name: "FK_RoomAmenities_Amenities_AmenitiesID1",
+                        column: x => x.AmenitiesID1,
+                        principalTable: "Amenities",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RoomAmenities_Room_RoomID",
+                        column: x => x.RoomID,
+                        principalTable: "Room",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelRoom_RoomID",
+                table: "HotelRoom",
+                column: "RoomID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomAmenities_AmenitiesID1",
+                table: "RoomAmenities",
+                column: "AmenitiesID1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomAmenities_RoomID",
+                table: "RoomAmenities",
+                column: "RoomID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HotelRoom");
+
+            migrationBuilder.DropTable(
+                name: "RoomAmenities");
+
+            migrationBuilder.DropTable(
+                name: "Hotel");
+
+            migrationBuilder.DropTable(
+                name: "Amenities");
+
+            migrationBuilder.DropTable(
+                name: "Room");
+        }
+    }
+}
