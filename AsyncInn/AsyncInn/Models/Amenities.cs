8c8a1885c1638c550433a134bd168d92200c3dcc
Matthew Johnson <matthew.james.johnson@hotmail.com>
2020-04-06 09:44:49 -0700

    Secrets built out and added to Startup. Additional comments added to all the models.

--- AsyncInn/AsyncInn/Models/Amenities.cs
+++ AsyncInn/AsyncInn/Models/Amenities.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace AsyncInn.Models
-{
+{   //This is a regular table.
     public class Amenities
-    {
+    {   /// <summary>
+    /// ID is the Amenities Primary Key, Name is the name of the amenities. 
+    /// </summary>
         public int ID { get; set; }
         public string Name { get; set; }
 
-        // nav properies
+        // These are the Nav Properies, this model links to RoomAmenities. 
         List<RoomAmenities> RoomAmenities = new List<RoomAmenities>();
     }
 }
